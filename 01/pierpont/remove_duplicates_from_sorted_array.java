7fe3b21033942c05e8578c1238759176f587efd5
Pierpont <linhaiqinlwtx@outlook.com>
2017-11-28 20:49:15 +0800

    pierpont v11.28.01

---
+++ 01/pierpont/remove_duplicates_from_sorted_array.java
@@ -0,0 +1,36 @@
+
+public class number_one {
+	
+	public static void main(String[] arge) {
+		int[] A = {1,1,2,2,2,2,3,4,2,4,5,6,4,6,6};
+		
+		int length = RDuplicates(A);
+		
+		System.out.println("length = " + length);
+		for (int i = 0;i < length;i++) {
+			System.out.print(A[i] + ",");
+		}
+	}
+	
+	static int RDuplicates(int[] A) {
+		int length = A.length;
+		
+		for (int i = 0;i < length;i++) {
+			for(int j = i + 1;j < length;) {
+				if (A[j] == A[i]) {
+					int a = A[j];
+					A[j] = A[length - 1];
+					A[length - 1] = a;
+					length--;
+				}
+				
+				if (A[j] != A[i]) {
+					j++;
+				} 
+			}
+		}
+		return length;
+	}
+}
+
+
